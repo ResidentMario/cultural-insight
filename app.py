6d1abb88eed1e22c89cd7b851d2b1a037080bdef
ResidentMario <aleksey.bilogur@gmail.com>
2015-11-08 16:27:31 -0500

    Issues appearing.

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def login():
 ###################################
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port), debug=True)
+	app.run(host='0.0.0.0', port=int(port))
