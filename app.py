7a11c99ca75a1fcdc2ab857cf837b4d1f2aa3912
ResidentMario <aleksey.bilogur@gmail.com>
2015-11-10 02:28:11 -0500

    Keep forgetting to remove the debug flag.

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def dashboard():
 ###################################
 port = os.getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port), debug=True)
+	app.run(host='0.0.0.0', port=int(port))
